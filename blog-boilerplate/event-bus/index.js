7e4dd8efd5a80eba53b7f244ce5685e97f4c462b
zahariaca <zaharia.c.alexandru@gmail.com>
2023-04-21 22:29:14 +0300

    Sections 1-4 of course

---
+++ blog-boilerplate/event-bus/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    console.log('Adding to events:', event)
+    events.push(event);
+
+    const {type} = event;
+    console.log(`Received event: ${type}`);
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
+    res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+    console.log('GET events called:', events)
+    res.send(events);
+});
+
+
+app.listen(4005, () => {
+    console.log('Listening on port 4005');
+});
