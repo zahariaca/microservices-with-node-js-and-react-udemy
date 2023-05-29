21dbe7ed180a842deb3dd519b040fb627f0f6baf
zahariaca <zaharia.c.alexandru@gmail.com>
2023-05-29 19:25:59 +0300

    Section 9: implements current-user
    Adds middleware to check jwt and set a currentUser on the request
    Adds not authorized error and midleware to check if the user is authorized

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,22 +1,15 @@
 import express, {Request, Response} from 'express';
-import jwt from "jsonwebtoken";
+import {currentUser} from "../middlewares/current-user";
+import {requireAuth} from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    if (!req.session?.jwt) { // ? equivalent to !req.session || !req.session.jwt
-        return res.send({currentUser: null});
-    }
-
-    try {
-        const payload = jwt.verify(
-            req.session.jwt,
-            process.env.JWT_KEY!
-        );
-        return res.send({currentUser: payload});
-    } catch (err) {
-        res.send({currentUser: null});
-    }
-});
+router.get(
+    '/api/users/currentuser',
+    currentUser,
+    requireAuth,
+    (req: Request, res: Response) => {
+        res.send({currentUser: req.currentUser || null});
+    });
 
 export {router as currentUserRouter};
